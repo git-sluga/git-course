90af77c84cd92282fc91f208fb960d8a0d549039
Thomas A. Sluga <thomas@sluga.io>
2020-04-28 14:02:32 +0200

    math.js ready

--- math.js
+++ math.js
@@ -1,4 +1,10 @@
 function data(){
     let array = []; // 1000;
     // to be done
+    console.time("messung");
+    for(x = 0; x < 1000; x++){
+        array[x] = x;
+    }
+    console.timeEnd("messung");
 }
+data();
