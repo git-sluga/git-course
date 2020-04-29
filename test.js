25ddd223aa45ed9314f55d564e2776a144c2c6aa
git-sluga <git@sluga.io>
2020-04-29 12:17:40 +0200

    cleanup

--- test.js
+++ test.js
@@ -1,4 +1,8 @@
 console.log("test");
+<<<<<<< HEAD
 console.log("irgendwas");
 console.log("base ....");
 console.log("abc");
+=======
+console.log("rebase");
+>>>>>>> parent of 648c37f... test.js second update
