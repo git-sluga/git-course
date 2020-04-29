39ead89ce62eb0fd2b2bc29763104032a10c40ec
git-sluga <64479160+git-sluga@users.noreply.github.com>
2020-04-29 15:31:32 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,1 @@
-console.log("test");
-<<<<<<< HEAD
-console.log("irgendwas");
-console.log("base ....");
-console.log("abc");
-=======
-console.log("rebase");
->>>>>>> parent of 648c37f... test.js second update
+console.log("final file");
